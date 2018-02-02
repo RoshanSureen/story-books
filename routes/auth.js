c7b0d2393a45ee277508e2245b34a88e44918a18
Roshan Sureen <roshansureen297@gmail.com>
2018-02-02 23:03:42 +0400

    successfully created google oauth call

---
+++ routes/auth.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var router = express.Router();
+var passport = require("passport");
+
+router.get(
+  "/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+module.exports = router;
