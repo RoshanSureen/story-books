154a715fe859dcc6d70d7f738476315986f474cf
Roshan Sureen <roshansureen297@gmail.com>
2018-02-02 22:24:18 +0400

    created a keys.js file

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongoURI: "mongodb://roshan:roshan@ds223738.mlab.com:23738/story-books-dev",
+  googleClientID:
+    "319381610910-i67ckjnrsggroscogs78mp193sjb8ui8.apps.googleusercontent.com",
+  googleClientSecret: "FbpP5WYKKUTADhT2Y4B0qirn"
+};
