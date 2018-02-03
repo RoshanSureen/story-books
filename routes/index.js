1e8299fc7bd76fe993430125f008ebdb8e332b7d
Roshan Sureen <roshansureen297@gmail.com>
2018-02-03 12:26:56 +0400

    created the welcome page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.get("/dashboard", (req, res, next) => {
+  res.json({
+    comifrmation: "SUCCESS",
+    message: "DASHBOARD"
+  });
 });
 
 module.exports = router;
